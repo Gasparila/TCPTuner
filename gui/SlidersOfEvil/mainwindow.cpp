d3ce9114b3074575b4056ccaa7e622cab0c5f58f
Luke Hsiao <luke.w.hsiao@gmail.com>
2016-02-02 21:52:38 -0800

    Switched code to Google C++ style

--- gui/SlidersOfEvil/mainwindow.cpp
+++ gui/SlidersOfEvil/mainwindow.cpp
@@ -5,26 +5,22 @@
 
 using namespace std;
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+MainWindow::MainWindow(QWidget* parent) :
+  QMainWindow(parent),
+  ui(new Ui::MainWindow) {
+  ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() {
+  delete ui;
 }
 
-void MainWindow::on_checkBox_toggled(bool checked)
-{
+void MainWindow::on_checkBox_toggled(bool checked) {
   if (checked) {
     system("echo -n 1 > /sys/module/tcp_evil/parameters/tcp_friendliness");
   }
   else {
     system("echo -n 0 > /sys/module/tcp_evil/parameters/tcp_friendliness");
   }
-  cout << "TCP Friendliness: ";
   system("cat /sys/module/tcp_evil/parameters/tcp_friendliness");
 }
